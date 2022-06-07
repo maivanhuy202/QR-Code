260f0100a2ffd837e6e04e31d6a31eaef1a0086d
maivanhuy202 <vanhuy112002qn@gmail.com>
2022-06-07 08:46:48 +0700

    Update 7/6/2022

--- app/src/main/java/com/example/bottom_menu/main/SettingFragment.java
+++ app/src/main/java/com/example/bottom_menu/main/SettingFragment.java
@@ -4,16 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
-
 import com.example.bottom_menu.R;
 
 public class SettingFragment extends Fragment {
 
+    ImageView share, rule, dark, report;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        share = view.findViewById(R.id.shareApp);
+        rule = view.findViewById(R.id.terms_of_use);
+        dark = view.findViewById(R.id.dark_mode);
+        report = view.findViewById(R.id.report);
+
+        share.setOnClickListener(view1 -> {
+
+        });
+        rule.setOnClickListener(view1 -> {
+
+        });
+        dark.setOnClickListener(view1 -> {
+
+        });
+        report.setOnClickListener(view1 -> {
+
+        });
+
+        return view;
     }
+
+
 }
